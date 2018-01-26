8b1448b2ba1e1949ec69d8d00128bc1d727c6c4a
Sebastian Stulz <sebastian.stulz@maxdome.de>
2018-01-26 15:02:35 +0100

    Set index_files to optional - otherwise it is not possible to let the parameter unset for a location

--- manifests/resource/location.pp
+++ manifests/resource/location.pp
@@ -162,7 +162,7 @@ define nginx::resource::location (
   String $server                                       = undef,
   Optional[String] $www_root                           = undef,
   Optional[String] $autoindex                          = undef,
-  Array $index_files                                   = [],
+  Optional[Array] $index_files                         = undef,
   Optional[String] $proxy                              = undef,
   Optional[String] $proxy_redirect                     = $::nginx::proxy_redirect,
   String $proxy_read_timeout                           = $::nginx::proxy_read_timeout,
